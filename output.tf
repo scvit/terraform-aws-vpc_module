09b36c3bbb80e10ca13a0e05beb57c5cf2a09ffa
scvit <myscvit@gmail.com>
2024-05-17 14:43:13 +0900

    0.0.3

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-output "vpc_cidr" {
-  value = aws_vpc.this.cidr_block   
-}
+# output "vpc_cidr" {
+#   value = aws_vpc.this.cidr_block   
+# }
 
 output "vpc_id" {
     value = aws_vpc.this.id
--- variable.tf
+++ variable.tf
@@ -1,3 +1,3 @@
 variable "vpc_cidr" {}
 
-# variable "vpc_name" {}
+variable "vpc_name" {}
