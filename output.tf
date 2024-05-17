b257490555b323a20bdc34cb9ca39df52db6f979
scvit <myscvit@gmail.com>
2024-05-17 15:31:41 +0900

    0.0.3 module change

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-# output "vpc_cidr" {
-#   value = aws_vpc.this.cidr_block   
-# }
-#
-#
-#
+output "vpc_cidr" {
+  value = aws_vpc.this.cidr_block   
+}
+
+
+
 output "vpc_id" {
     value = aws_vpc.this.id
 }
