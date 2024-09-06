864b240791efe912e23f701f6553b337ee20077b
scvit <88419463+scvit@users.noreply.github.com>
2024-09-06 15:42:37 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.12.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+
 resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = "true"
