42163b2dd73324bd03cd38b7808f70aab3a0ba32
scvit <88419463+scvit@users.noreply.github.com>
2024-09-06 15:47:55 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.12.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
-
 
 resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
